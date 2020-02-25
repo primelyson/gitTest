6650695ef86bc641b449939f75c64959c69ddb1b
primely <primely@naver.com>
2020-02-25 13:51:48 +0900

    11

--- src/gitTest/GitTest.java
+++ src/gitTest/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요11111반값습니다55555555555222223331177889555779999");
-		System.out.println("1111111111111222");
+		System.out.println("1111111111111222555");
 	}
 
 }
